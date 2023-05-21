2de347913d7b1f19e709aea497f4b211a823d821
Rui Guo <67830284+zkyguo@users.noreply.github.com>
2023-05-21 01:26:50 -0400

    Finish basic chat response deserialize

--- Plugins/SimpleChatGPT/Source/SimpleChatGPT/Private/Core/SimpleChatGPTMethod.cpp
+++ Plugins/SimpleChatGPT/Source/SimpleChatGPT/Private/Core/SimpleChatGPTMethod.cpp
@@ -15,6 +15,17 @@ namespace SimpleChatGPTMethod
 		return TEXT("");
 	}
 
+	EChatGPTModel StringToEChatGPTModel(const FString& model)
+	{
+		if(model.Equals("text_davinci-003"))
+		{
+			return EChatGPTModel::DAVINCI_003;
+		}
+
+		return EChatGPTModel::NONE;
+		
+	}
+
 	void FChatGPTCompletionParamToString(const FChatGPTCompletionParam param, FString& Outjson)
 	{
 		TSharedPtr<TJsonWriter<TCHAR, TCondensedJsonPrintPolicy<TCHAR>>> JsonWriter =
@@ -34,5 +45,80 @@ namespace SimpleChatGPTMethod
 		}
 		JsonWriter->WriteObjectEnd();//}
 		JsonWriter->Close();
+	}
+
+	void StringToFChatGPTCompletionResponse(const FString& Json, FChatGPTCompletionResponse& OutResponse)
+	{
+		TSharedRef<TJsonReader<>> JsonReader = TJsonReaderFactory<>::Create(Json);
+		TSharedPtr<FJsonValue> ReadRoot;
+
+		if(FJsonSerializer::Deserialize(JsonReader, ReadRoot))
+		{
+			if(TSharedPtr<FJsonObject> InJsonObject = ReadRoot->AsObject())
+			{
+				OutResponse.ID = InJsonObject->GetStringField(TEXT("id"));
+				OutResponse.Object = InJsonObject->GetStringField(TEXT("object"));
+				OutResponse.Created = InJsonObject->GetIntegerField(TEXT("created"));
+				OutResponse.Mode = StringToEChatGPTModel(InJsonObject->GetStringField(TEXT("model")));
+
+				if(TSharedPtr<FJsonObject> UsageJsonObject = InJsonObject->GetObjectField(TEXT("usage")))
+				{
+					OutResponse.Usage.PromptTokens = UsageJsonObject->GetIntegerField(TEXT("prompt_tokens"));
+					OutResponse.Usage.CompletionTokens = UsageJsonObject->GetIntegerField(TEXT("completion_tokens"));
+					OutResponse.Usage.TotalTokens = UsageJsonObject->GetIntegerField(TEXT("total_tokens"));
+				}
+
+				const TArray<TSharedPtr<FJsonValue>>* OutArray = NULL;
+				if(InJsonObject->TryGetArrayField(TEXT("choices"), OutArray))
+				{
+					for (auto&Tmp : *OutArray)
+					{
+						if(TSharedPtr<FJsonObject> ChoiceJsonObject = Tmp->AsObject() )
+						{
+							FChatGPTChoice &Choice = OutResponse.Choices.AddDefaulted_GetRef();
+							Choice.Text = ChoiceJsonObject->GetStringField(TEXT("text"));
+							Choice.Index = ChoiceJsonObject->GetIntegerField(TEXT("index"));
+							Choice.FinishReason = ChoiceJsonObject->GetStringField(TEXT("finish_reason"));
+
+							if(TSharedPtr<FJsonObject> LogprobsJsonObject = ChoiceJsonObject->GetObjectField(TEXT("logprobs")))
+							{
+								const  TArray<TSharedPtr<FJsonValue>>* tokenArray = NULL;
+								if(LogprobsJsonObject->TryGetArrayField(TEXT("tokens"), tokenArray))
+								{
+									for (auto& tokenTmp : *tokenArray)
+									{
+										Choice.logProbs.Tokens.Add(tokenTmp->AsString());
+									}
+								}
+
+								const  TArray<TSharedPtr<FJsonValue>>* logArray = NULL;
+								if (LogprobsJsonObject->TryGetArrayField(TEXT("tokens_logprobs"), logArray))
+								{
+									for (auto& logTmp : *logArray)
+									{
+										Choice.logProbs.Tokens_logprobs.Add(logTmp->AsNumber());
+									}
+								}
+
+								const  TArray<TSharedPtr<FJsonValue>>* offsetArray = NULL;
+								if (LogprobsJsonObject->TryGetArrayField(TEXT("text_offset"), offsetArray))
+								{
+									for (auto& offsetTmp : *offsetArray)
+									{
+										int32 &InTextOffset = Choice.logProbs.Text_offset.AddDefaulted_GetRef();
+										if(offsetTmp->TryGetNumber(InTextOffset) )
+										{
+											
+										}
+									}
+								}
+							}
+
+						}
+					}
+				}
+			}
+		}
+
 	}
 }
--- Plugins/SimpleChatGPT/Source/SimpleChatGPT/Public/Core/SimpleChatGPTMethod.h
+++ Plugins/SimpleChatGPT/Source/SimpleChatGPT/Public/Core/SimpleChatGPTMethod.h
@@ -6,6 +6,9 @@
 namespace SimpleChatGPTMethod
 {
 	FString EChatGPTModelToString(EChatGPTModel	model);
+	EChatGPTModel StringToEChatGPTModel(const FString& model);
+
 	void FChatGPTCompletionParamToString(const FChatGPTCompletionParam param, FString &Outjson);
+	void StringToFChatGPTCompletionResponse(const FString &Json, FChatGPTCompletionResponse& OutResponse);
 
 }
